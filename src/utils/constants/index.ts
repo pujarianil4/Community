39ff317ee1624b4f8f4cba950ab4ed97f805254c
Mangesh Landge <mangeshbkwsu@gmail.com>
2024-08-12 17:41:50 +0530

    remove space from the end in msg

--- src/utils/constants/index.ts
+++ src/utils/constants/index.ts
@@ -1,1 +1,1 @@
-export const sigMsg = `Sign this message to prove you have access to this wallet in order to sign in to Community. This won't cost you any Gas. Date: ${Date.now()} `;
+export const sigMsg = `Sign this message to prove you have access to this wallet in order to sign in to Community. This won't cost you any Gas. Date: ${Date.now()}`;
