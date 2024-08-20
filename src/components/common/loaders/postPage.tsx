4b45a39da0af8f51028ee397021907398dea1ec2
Mangesh Landge <mangeshbkwsu@gmail.com>
2024-08-20 11:21:03 +0530

    ui updates

---
+++ src/components/common/loaders/postPage.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function PostPageLoader() {
+  return (
+    <div className='post_page_loader'>
+      <div className='skeleton'></div>
+      <div className='skeleton'></div>
+      <div className='skeleton'></div>
+    </div>
+  );
+}
