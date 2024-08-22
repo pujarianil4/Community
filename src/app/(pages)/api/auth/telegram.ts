b9cd6d1b4ff6dab64814decddbff242a709097e8
DAppArtisan <141224058+DAppArtisan@users.noreply.github.com>
2024-08-22 16:18:08 +0530

    update

--- src/app/(pages)/api/auth/telegram.ts
+++ src/app/(pages)/api/auth/telegram.ts
@@ -9,7 +9,7 @@
 //   const authData: AuthData = req.query as unknown as AuthData;
 
 //   // Your bot token
-//   const botToken = "7280759352:AAHqOAaBc9weSxHG7o1t6tFhbxN8l3QBG_c";
+//   const botToken = "";
 
 //   // Create a secret from the bot token
 //   const secret = crypto.createHash("sha256").update(botToken).digest();
