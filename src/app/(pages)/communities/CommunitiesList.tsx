a400f2a5258b0642af58408568e18f170ce2f5dc
Mangesh Landge <mangeshbkwsu@gmail.com>
2024-09-09 23:49:03 +0530

    explore communiyt page added

---
+++ src/app/(pages)/communities/CommunitiesList.tsx
@@ -0,0 +1,34 @@
+"use client";
+import React from "react";
+import useAsync from "@/hooks/useAsync";
+import { fetchCommunities } from "@/services/api/api";
+import CardList from "@/components/cardList";
+import { ICommunity } from "@/utils/types/types";
+
+interface List {
+  value: string;
+  title: string;
+}
+
+export default function CommunitiesList() {
+  const {
+    isLoading,
+    data: communityData,
+    refetch,
+    callFunction,
+  } = useAsync(fetchCommunities, "followers");
+  console.log("COMMUNITY_DATA", communityData);
+  const handleFilter = (filter: List) => {
+    callFunction(fetchCommunities, filter.value);
+  };
+  return (
+    <>
+      <CardList
+        cardListData={communityData as ICommunity[]}
+        type='c'
+        showFilters
+        handleFilter={(filter) => handleFilter(filter)}
+      />
+    </>
+  );
+}
