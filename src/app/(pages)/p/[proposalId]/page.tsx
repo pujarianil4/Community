d944137e6852f9be1c7f845b3b7d6be3b9815726
Mangesh Landge <mangeshbkwsu@gmail.com>
2024-09-20 13:54:39 +0530

    proposal ui update

--- src/app/(pages)/p/[proposalId]/page.tsx
+++ src/app/(pages)/p/[proposalId]/page.tsx
@@ -1,22 +1,18 @@
-"use client";
 import React from "react";
 import "./index.scss";
 import PageWraper from "@/components/Wrapers/PageWraper";
-import { fetchProposalByID } from "@/services/api/api";
 import ProposalDetails from "./proposalDetails";
 
 interface IProps {
   params: any;
 }
 
-// TODO: Create seperate button for Active button
 export default async function ProposalPage({ params }: IProps) {
   const { proposalId } = params;
-  const proposalData = await fetchProposalByID(proposalId);
 
   return (
     <PageWraper>
-      <ProposalDetails proposal={proposalData} />
+      <ProposalDetails proposalId={proposalId} />
     </PageWraper>
   );
 }
--- src/app/(pages)/p/[proposalId]/proposalDetails.tsx
+++ src/app/(pages)/p/[proposalId]/proposalDetails.tsx
@@ -1,74 +1,86 @@
-// "use client";
+"use client";
 import React, { useEffect } from "react";
 import { ShareIcon } from "@/assets/icons";
 import CButton from "@/components/common/Button";
 import MarkdownRenderer from "@/components/common/MarkDownRender";
 import { getImageSource, timeAgo } from "@/utils/helpers";
-import { IProposal } from "@/utils/types/types";
 import Image from "next/image";
 import Link from "next/link";
 import useRedux from "@/hooks/useRedux";
+import useAsync from "@/hooks/useAsync";
+import { fetchProposalByID } from "@/services/api/api";
 
 interface IProps {
-  proposal: IProposal;
+  proposalId: string;
 }
-export default function ProposalDetails({ proposal }: IProps) {
-  const { cta: time, title, isVoted, desc, user, community } = proposal;
+export default function ProposalDetails({ proposalId }: IProps) {
+  const { isLoading, data: proposalData } = useAsync(
+    fetchProposalByID,
+    proposalId
+  );
 
-  console.log("Proposal", proposal);
-  //
   const [{ dispatch, actions }] = useRedux();
 
-  // useEffect(() => {
-  dispatch(actions.setProposalVote(isVoted));
-  // }, [proposal]);
+  useEffect(() => {
+    dispatch(actions.setProposalVote(proposalData?.isVoted));
+  }, [proposalData]);
 
   return (
-    <main className='proposal_page_container'>
-      <section className='proposal_content'>
-        <h1>Proposal</h1>
-        <section className='proposal_section'>
-          <div className='proposal_head'>
-            <h2>{title}</h2>
-            <div>
-              <CButton className='active'>Active</CButton>
-              <p className='post_time'> {timeAgo(time)}</p>
-            </div>
-          </div>
-          <div className='user_head'>
-            <div>
-              <Link
-                href={`c/${community?.username}`}
-                as={`/c/${community?.username}`}
-              >
-                <Image
-                  src={getImageSource(community?.img?.pro, "c")}
-                  alt={community?.name || "community"}
-                  width={24}
-                  height={24}
-                  loading='lazy'
-                />
-                <p>{community?.username}</p>
-              </Link>
-              <Link href={`u/${user?.username}`} as={`/u/${user?.username}`}>
-                <Image
-                  src={getImageSource(user?.img?.pro, "u")}
-                  alt={user?.name || "user"}
-                  width={24}
-                  height={24}
-                  loading='lazy'
-                />
-                <p>{user?.username}</p>
-              </Link>
-            </div>
-            <CButton>
-              <ShareIcon />
-              Share
-            </CButton>
-          </div>
-          <MarkdownRenderer markdownContent={desc} />
-        </section>
-      </section>
-    </main>
+    <>
+      {proposalData && (
+        <main className='proposal_page_container'>
+          <section className='proposal_content'>
+            <h1>Proposal</h1>
+            <section className='proposal_section'>
+              <div className='proposal_head'>
+                <h2>{proposalData?.title}</h2>
+                <div>
+                  <CButton className='active'>Active</CButton>
+                  <p className='post_time'> {timeAgo(proposalData?.cta)}</p>
+                </div>
+              </div>
+              <div className='user_head'>
+                <div>
+                  <Link
+                    href={`c/${proposalData?.community?.username}`}
+                    as={`/c/${proposalData?.community?.username}`}
+                  >
+                    <Image
+                      src={getImageSource(
+                        proposalData?.community?.img?.pro,
+                        "c"
+                      )}
+                      alt={proposalData?.community?.name || "community"}
+                      width={24}
+                      height={24}
+                      loading='lazy'
+                    />
+                    <p>{proposalData?.community?.username}</p>
+                  </Link>
+                  <Link
+                    href={`u/${proposalData?.user?.username}`}
+                    as={`/u/${proposalData?.user?.username}`}
+                  >
+                    <Image
+                      src={getImageSource(proposalData?.user?.img?.pro, "u")}
+                      alt={proposalData?.user?.name || "user"}
+                      width={24}
+                      height={24}
+                      loading='lazy'
+                    />
+                    <p>{proposalData?.user?.username}</p>
+                  </Link>
+                </div>
+                <CButton>
+                  <ShareIcon />
+                  Share
+                </CButton>
+              </div>
+              <MarkdownRenderer markdownContent={proposalData?.desc} />
+            </section>
+          </section>
+        </main>
+      )}
+    </>
   );
 }
